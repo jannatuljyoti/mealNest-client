f0d2c82be1655ce2fd77307e728c2589e957757c
jannatuljyoti <jannatuljyoti085@gmail.com>
2025-07-09 15:36:44 +0600

    first commit

---
+++ src/pages/shared/mealnest/MealNestLogo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from '../../../assets/download (3).jpeg'
+
+const MealNestLogo = () => {
+    return (
+        <div>
+            <img src={logo} alt="" />
+            <p>MealNest</p>
+        </div>
+    );
+};
+
+export default MealNestLogo;
