445286e34789c8a98de4019b9ce4a32249f7d9f8
jannatuljyoti <jannatuljyoti085@gmail.com>
2025-07-10 18:47:05 +0600

    added login page

--- src/pages/JoinUs/Login.jsx
+++ src/pages/JoinUs/Login.jsx
@@ -1,28 +1,78 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
+import useAuth from '../../hooks/useAuth';
+import { useNavigate } from 'react-router';
+import Swal from 'sweetalert2';
+import axios from 'axios';
+import { FcGoogle } from 'react-icons/fc';
 
 const Login = () => {
-    const {register, handleSubmit,formState:{errors}} = useForm();
+    const {register, handleSubmit, formState: {errors}} = useForm();
+    const {signIn, googleSignIn} = useAuth();
+    const navigate = useNavigate();
+
+    const onSubmit = async(data)=>{
+        try{
+            await signIn(data.email, data.password);
+            Swal.fire('Login Successful', '', 'success');
+            navigate('/');
+        } catch (err){
+            Swal.fire('Login Failed', err.message, 'error');
+        }
+    };
+
+    const handleGoogle = async()=>{
+        try{
+            const result = await googleSignIn();
+            const user = result.user;
+
+            const saveUser = {
+                name: user.displayName,
+                email: user.email,
+                photoURL: user.photoURL,
+                badge: 'Bronze',
+                role: 'user',
+            };
+
+            await axios.post('/api/users', saveUser);
+            Swal.fire('Google Login Successful', '', 'success');
+            navigate('/');
+        }catch(err){
+            Swal.fire('Google Login Failed', err.message, 'error');
+        }
+    };
+
+
     return (
-        <div>
-            <form onSubmit={handleSubmit(onSubmit)}>
+        <div className='bg-base-200 p-6 rounded-lg shadow '>
+            <h2 className='text-2xl font-semibold text-center mb-4'>Login Your Account</h2>
+            <form onSubmit={handleSubmit(onSubmit)} className='space-y-3'>
+                 <label className="label font-bold text-xl">Email:</label>
                  <input type="email"
                   {...register('email')}
-                   className="input" placeholder="Email" /> 
+                   className="input input-bordered w-full" placeholder="Email"required/> 
 
-                 
+          <label className="label font-bold text-xl">Password:</label>       
           <input type="password" 
           {...register('password',{
             required:true,
             minLength: 6 })}
-           className="input" placeholder="Password" />
+           className="input input-bordered w-full" placeholder="Password" />
            {
-            errors.password?.type=== 'required' && <p className='text-red-600'>Password is required</p>
-           }
+            errors.password?.type=== 'required' && (<p className='text-red-600'>Password is required</p>
+           )}
            {
-            errors.password?.type=== 'minLength' && <p className='text-red-600'>Password is required</p>
-           }
+            errors.password?.type=== 'minLength' && (<p className='text-red-600'>Password must be at least 6 characters</p>
+           )}
+
+           <p className='text-xl mb-5'>Forget Password?</p>
+
+           <button type='submit' className='btn bg-[#ec644b]  w-full text-white'>Login</button>
+
             </form>
+
+            <div className='divider'>OR</div>
+            <button onClick={handleGoogle} className='btn  text-white bg-[#0c6c7c] w-full'>Continue with <FcGoogle className='w-5 h-5'/>Google</button>
         </div>
     );
 };
