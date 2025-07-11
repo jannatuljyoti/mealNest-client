406d6adc6c6049c18c63e92c45e9dbb327cb8394
jannatuljyoti <jannatuljyoti085@gmail.com>
2025-07-12 00:48:31 +0600

    added oser dashboard

--- src/pages/UserAdmin/UserDashboardLayout.jsx
+++ src/pages/UserAdmin/UserDashboardLayout.jsx
@@ -1,61 +1,84 @@
 import React from 'react';
 import { NavLink, Outlet } from 'react-router';
-
-
+import {
+  IoPersonOutline,
+  IoFastFoodOutline,
+  IoChatbubbleEllipsesOutline,
+  IoCardOutline,
+  IoGridOutline
+} from 'react-icons/io5';
+import MealNestLogo from '../shared/mealnest/MealNestLogo';
 
 const UserDashboardLayout = () => {
   return (
     <div className="flex flex-col md:flex-row min-h-screen">
       {/* Sidebar */}
-      <aside className="w-full md:w-64 bg-gray-100 p-6 shadow-md">
-        <h2 className="text-2xl font-bold text-center mb-6">User Dashboard</h2>
+      <aside className="w-full md:w-64 bg-gray-800 text-white p-6 shadow-md">
+
+     <div className='mb-7'>
+        <MealNestLogo/>
+    </div>
+
+        <h2 className="text-2xl font-bold text-center mb-6 flex items-center justify-center gap-2">
+  <IoGridOutline size={24} />
+  User Dashboard
+</h2>
         <nav>
           <ul className="space-y-3">
             <li>
               <NavLink
-                to="/user-dashboard"
-                className={({ isActive }) =>
-                  isActive
-                    ? "text-white bg-blue-600 px-4 py-2 rounded block"
-                    : "text-gray-700 hover:bg-gray-200 px-4 py-2 rounded block"
-                }
-              >
-                My Profile
-              </NavLink>
+  to="/user-dashboard"
+  end
+  className={({ isActive }) =>
+    isActive
+      ? "flex items-center gap-2 text-orange-400 font-semibold"
+      : "flex items-center gap-2 text-white hover:text-orange-300"
+  }
+>
+  <IoPersonOutline size={20} />
+  My Profile
+</NavLink>
+
             </li>
+
             <li>
               <NavLink
-                to="/user-dashboard/requested-meals"
+                to="/user-dashboard/request-meals"
                 className={({ isActive }) =>
                   isActive
-                    ? "text-white bg-blue-600 px-4 py-2 rounded block"
-                    : "text-gray-700 hover:bg-gray-200 px-4 py-2 rounded block"
+                    ? "flex items-center gap-2 text-orange-400 font-semibold"
+                    : "flex items-center gap-2 text-white hover:text-orange-300"
                 }
               >
+                <IoFastFoodOutline size={20} />
                 Requested Meals
               </NavLink>
             </li>
+
             <li>
               <NavLink
                 to="/user-dashboard/my-reviews"
                 className={({ isActive }) =>
                   isActive
-                    ? "text-white bg-blue-600 px-4 py-2 rounded block"
-                    : "text-gray-700 hover:bg-gray-200 px-4 py-2 rounded block"
+                    ? "flex items-center gap-2 text-orange-400 font-semibold"
+                    : "flex items-center gap-2 text-white hover:text-orange-300"
                 }
               >
+                <IoChatbubbleEllipsesOutline size={20} />
                 My Reviews
               </NavLink>
             </li>
+
             <li>
               <NavLink
                 to="/user-dashboard/payment-history"
                 className={({ isActive }) =>
                   isActive
-                    ? "text-white bg-blue-600 px-4 py-2 rounded block"
-                    : "text-gray-700 hover:bg-gray-200 px-4 py-2 rounded block"
+                    ? "flex items-center gap-2 text-orange-400 font-semibold"
+                    : "flex items-center gap-2 text-white hover:text-orange-300"
                 }
               >
+                <IoCardOutline size={20} />
                 Payment History
               </NavLink>
             </li>
@@ -64,7 +87,7 @@ const UserDashboardLayout = () => {
       </aside>
 
       {/* Main content */}
-      <main className="flex-1 p-6 bg-white">
+      <main className="flex-1 p-6 bg-gray-100">
         <Outlet />
       </main>
     </div>
--- src/pages/shared/Navbar/Navbar.jsx
+++ src/pages/shared/Navbar/Navbar.jsx
@@ -3,10 +3,12 @@ import { Link, NavLink, useNavigate } from 'react-router';
 import MealNestLogo from '../mealnest/MealNestLogo';
 import { IoIosNotificationsOutline } from 'react-icons/io';
 import useAuth from '../../../hooks/useAuth';
+import useAdmin from '../../../hooks/useAdmin'
 
 const Navbar = () => {
   const {user, logOut} = useAuth();
   const navigate = useNavigate();
+   const [isAdmin] = useAdmin();
 
   const handleLogOut= async()=>{
     await logOut();
@@ -28,56 +30,88 @@ const Navbar = () => {
     </>
     return (
         <div className="navbar bg-base-100 shadow-sm">
-  <div className="navbar-start">
-    <div className="dropdown">
-      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
-      </div>
-      <ul
-        tabIndex={0}
-        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
-       {navItems}
-      </ul>
-    </div>
-    <Link to="/" className="btn btn-ghost text-xl">
-      <MealNestLogo></MealNestLogo>
-    </Link>
-
-
-  </div>
-  <div className="navbar-center hidden lg:flex">
-    <ul className="menu menu-horizontal px-1">
-     {navItems}
-    </ul>
-  </div>
-
-
-  <div className="navbar-end gap-2">
-      <p><IoIosNotificationsOutline className='w-8 h-8 text-[#ec644b]'/></p>
-
-
-      {user?(
-        <div className='dropdown dropdown-end'>
-          <div tabIndex={0} className='avatar online cursor-pointer'>
-            <div className='w-10 rounded-full'>
-                  <img src={user?.photoURL || '/user.png'} alt="Profile" />
-            </div>
+      <div className="navbar-start">
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
           </div>
-
           <ul
-          tabIndex={0}
-          className='menu menu-sm dropdown-content mt-3 z-10 p-2 shadow bg-base-100 rounded-box w-52'>
-
-            <li><span className='text-lg font-bold'>{user.displayName || 'user'}</span></li>
-            <li><Link to="/dashboard">Dashboard</Link></li>
-            <li><button onClick={handleLogOut}>Logout</button></li>
+            tabIndex={0}
+            className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow"
+          >
+            {navItems}
           </ul>
         </div>
-      ):(<Link to="/login" className="btn bg-[#ec644b] text-xl text-white">Join Us</Link>)}
+        <Link to="/" className="btn btn-ghost text-xl">
+          <MealNestLogo />
+        </Link>
+      </div>
 
-    
-  </div>
-</div>
+      <div className="navbar-center hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">{navItems}</ul>
+      </div>
+
+      <div className="navbar-end gap-2">
+        <p>
+          <IoIosNotificationsOutline className="w-8 h-8 text-[#ec644b]" />
+        </p>
+
+        {user ? (
+          <div className="dropdown dropdown-end">
+            <div tabIndex={0} className="avatar online cursor-pointer">
+              <div className="w-10 rounded-full">
+                <img src={user?.photoURL || '/user.png'} alt="Profile" />
+              </div>
+            </div>
+
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-10 p-2 shadow bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <span className="text-lg font-bold text-[#ec644b]">
+                  {user.displayName || 'user'}
+                </span>
+              </li>
+              <li>
+                {/* ✅ Conditional Dashboard Path */}
+                <Link
+                  to={isAdmin ? '/dashboard/profile' : '/user-dashboard'}
+                  className="text-sm font-bold text-[#ec644b]"
+                >
+                  Dashboard
+                </Link>
+              </li>
+              <li>
+                <button
+                  onClick={handleLogOut}
+                  className="text-sm font-bold text-[#ec644b]"
+                >
+                  Logout
+                </button>
+              </li>
+            </ul>
+          </div>
+        ) : (
+          <Link to="/login" className="btn bg-[#ec644b] text-xl text-white">
+            Join Us
+          </Link>
+        )}
+      </div>
+    </div>
     );
 };
 
